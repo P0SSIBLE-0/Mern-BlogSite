b6bc9bf37369400fd11c10d7b96c6354e13b5124
possible <sumitkumar66416@gmail.com>
2024-03-20 16:02:47 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -46,8 +46,6 @@ app.use((req, res, next) => {
 app.use(cors({
   origin: 'https://mern-blog-site-liart.vercel.app',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
 
 app.get('/post/:id', async(req, res) => {

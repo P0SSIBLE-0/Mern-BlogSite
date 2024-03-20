041f66bc63b913753df8ab40b6222a827888fb38
possible <sumitkumar66416@gmail.com>
2024-03-20 18:46:37 +0530

    fix cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import config from './config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/post': {
-        target: config.server_url,
+        target: 'https://mern-blog-site-api.vercel.app',
         changeOrigin: true,
       },
     },
